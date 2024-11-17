46d336df2d46fb607b60bc8cf38dd2d47706fccb
filtoid <philjeffes@hotmail.com>
2024-11-17 21:35:37 +0100

    initial checkin to verify

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mssql-integration-test
 A library for creating an integration testing db object, for an actual database.
+
+### Introduction
+
+This is designed to be a drop in replacement for the [MySql PyPi Project](https://pypi.org/project/mssql/): [Github](https://github.com/JoseRoberts87/mssql/tree/master), for use within a specific project - [Pyway](https://github.com/jasondcamp/pyway). There's no reason it won't work for other projects, but that was the original reason for creating this.
+
+### Installation
+As of now it is not installed in PyPi so you have install from Github:
+`pip install git+https://github.com/filtoid/mssql-integration-tests`
+
+### Usage
+```python
+from mssql-integration-tests import MsSql
+
+mssql = MsSql(host="<HOSTNAME>", port=<PORT>, db_name="<DATABASE_NAME>", username="<USER>", password="<PASSWORD>", driver="<DRIVER>")
+
+```
---
+++ mssql_integration_test/mssql.py
@@ -0,0 +1,36 @@
+import pyodbc
+
+class MsSql(Object):
+    def __init__(self, host, db_name, driver, username=None, password=None, port=1433):
+        if host is None:
+            raise ValueError("Host cannot be None")
+        
+        if db_name is None:
+            raise ValueError("Database Name cannot be None")
+
+        if driver is None:
+            raise ValueError("Driver cannot be None")
+
+        self.host = host
+        self.port = port
+        self.db_name = db_name
+        self.username = username
+        self.password = password
+        self.driver = driver
+
+        self.__engine__ = None
+        self.__conn__ = None
+
+    def connect(self):
+        if self.__conn__ is None:
+            conn_str  = f"DRIVER={{{self.driver}}};SERVER={self.host},{self.port};" \
+                        f"DATABASE={self.db_name};"
+            if self.username is not None and self.password is not None:
+                conn_str = f"{conn_str}UID={self.username};PWD={self.password};" \
+                            "TrustServerCertificate=yes;"
+            else:
+                conn_str = f"{conn_str}Trusted_Connection=yes;" \
+                            "TrustServerCertificate=yes;INTEGRATED " \
+                            "SECURITY=yes;ENCRYPT=no;"
+            self.__conn__ = pyodbc.connect(conn_str)
+        return self.__conn__
